c375642f6f2d02139294b47d855bfb878572a246
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-31 21:46:18 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,8 +23,16 @@ class InteractiveRecord
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
-    self
+    end
   end
-end
 
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
 end
